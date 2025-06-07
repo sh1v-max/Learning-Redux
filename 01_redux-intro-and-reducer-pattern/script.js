bd576be4fb55548b2a039517afc704c1365f030a
Shiv Shankar Singh <75415783+sh1v-max@users.noreply.github.com>
2025-06-07 20:12:56 +0530

    redux intro

--- 01_redux-intro-and-reducer-pattern/script.js
+++ 01_redux-intro-and-reducer-pattern/script.js
@@ -1,23 +1,41 @@
-let state = {
-  count: 0
+import { createStore } from 'redux'
+
+const initialState = {
+  post: 0,
+  name: 'Anurag Singh',
+  age: 26,
 }
 
-let prevState = state
+const INCREMENT = 'post/increment'
+const DECREMENT = 'post/decrement'
+const INCREASE_BY = 'post/increaseBy'
+const DECREASE_BY = 'post/decreaseBy'
 
-function increment(){
-  // we are mutating the state
-  // state.count = state.count + 1
-  // using this will update prevState too
-
-  // not mutating state
-  state = {count: state.count + 1}
+function reducer(state = initialState, action) {
+  switch (action.type) {
+    case INCREMENT:
+      return { ...state, post: state.post + 1 }
+    case DECREMENT:
+      return { ...state, post: state.post - 1 }
+    case INCREASE_BY:
+      return { ...state, post: state.post + action.payload }
+    case DECREASE_BY:
+      return { ...state, post: state.post - action.payload }
+    default:
+      return state
+  }
 }
 
-increment()
-console.log(state)
-increment()
-console.log(state)
-increment()
-console.log(state)
-increment()
-console.log(state)
+const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__?.())
+
+console.log(store)
+
+store.subscribe(() => {
+  console.log(store.getState())
+})
+
+
+store.dispatch({ type: INCREMENT })
+store.dispatch({ type: DECREMENT })
+store.dispatch({ type: INCREASE_BY, payload: 15 })
+store.dispatch({ type: DECREASE_BY, payload: 5 })
