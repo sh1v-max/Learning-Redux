5ea17b9b3c71bb5b2b43ce9355fb6d81180dd2bf
Shiv Shankar Singh <75415783+sh1v-max@users.noreply.github.com>
2025-06-07 18:01:53 +0530

    redux

--- 01_redux-intro-and-reducer-pattern/script.js
+++ 01_redux-intro-and-reducer-pattern/script.js
@@ -2,8 +2,15 @@ let state = {
   count: 0
 }
 
+let prevState = state
+
 function increment(){
-  state.count = state.count + 1
+  // we are mutating the state
+  // state.count = state.count + 1
+  // using this will update prevState too
+
+  // not mutating state
+  state = {count: state.count + 1}
 }
 
 increment()
