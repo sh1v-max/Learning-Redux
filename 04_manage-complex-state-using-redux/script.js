8c750b4fe87b53d9d70290391c2689cac9eb37a4
Shiv Shankar Singh <75415783+sh1v-max@users.noreply.github.com>
2025-06-13 12:54:16 +0530

    remove item

--- 04_manage-complex-state-using-redux/script.js
+++ 04_manage-complex-state-using-redux/script.js
@@ -9,11 +9,21 @@ const initialState = {
 
 // console.log(initialState.products)
 const CART_ADD_ITEM = 'cart/addItem'
+const CART_REMOVE_ITEM = 'cart/removeItem'
 
 function reducer(state = initialState, action) {
   switch (action.type) {
     case CART_ADD_ITEM:
       return { ...state, cartItems: [...state.cartItems, action.payload] }
+    default:
+      return state
+    case CART_REMOVE_ITEM:
+      return {
+        ...state,
+        cartItems: state.cartItems.filter(
+          (cartItem) => cartItem.productId !== action.payload.productId
+        ),
+      }
   }
 }
 
@@ -22,3 +32,7 @@ const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__?.())
 console.log(store)
 store.dispatch({ type: CART_ADD_ITEM, payload: { productId: 1, quantity: 1 } })
 store.dispatch({ type: CART_ADD_ITEM, payload: { productId: 12, quantity: 1 } })
+store.dispatch({ type: CART_ADD_ITEM, payload: { productId: 6, quantity: 1 } })
+store.dispatch({ type: CART_ADD_ITEM, payload: { productId: 9, quantity: 1 } })
+store.dispatch({ type: CART_REMOVE_ITEM, payload: { productId: 6} })
+store.dispatch({ type: CART_REMOVE_ITEM, payload: { productId: 9} })
