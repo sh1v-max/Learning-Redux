66a0d52e7ec244f33525a683e69c867a10caa8a1
Shiv Shankar Singh <75415783+sh1v-max@users.noreply.github.com>
2025-06-13 12:49:29 +0530

    add item

--- 04_manage-complex-state-using-redux/script.js
+++ 04_manage-complex-state-using-redux/script.js
@@ -7,11 +7,18 @@ const initialState = {
   wishList: [],
 }
 
-console.log(initialState.products)
+// console.log(initialState.products)
+const CART_ADD_ITEM = 'cart/addItem'
 
 function reducer(state = initialState, action) {
+  switch (action.type) {
+    case CART_ADD_ITEM:
+      return { ...state, cartItems: [...state.cartItems, action.payload] }
+  }
 }
 
 const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__?.())
 
-// console.log(store.getState())
+console.log(store)
+store.dispatch({ type: CART_ADD_ITEM, payload: { productId: 1, quantity: 1 } })
+store.dispatch({ type: CART_ADD_ITEM, payload: { productId: 12, quantity: 1 } })
